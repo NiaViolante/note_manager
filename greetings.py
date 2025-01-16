b054c624f2a07fde8b6dbde7c9108b0ceb250949
NiaViolante <qqruza5000@gmail.com>
2025-01-16 19:41:19 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username='Ivan' #имя пользователя
+title='Main' #заголовок заметки
+content='My notes' #описание заметки
+status='In progress' # статус заметки
+created_date='16-01-2025' # дата создания заметки
+issue_date='25-04-2025' # дата истечения заметки (дедлайн)
+print('Имя пользователя', username)
+print('Заголовок заметки', title)
+print('Описание заметки', content)
+print('Статус заметки', status)
+print('Дата создания', created_date)
+print('Дата истечения заметки (дедлайн)', issue_date)
