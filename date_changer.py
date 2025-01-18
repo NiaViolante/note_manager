bfc58fe0beab667d54456bf03998f9c1a9a1874d
NiaViolante <qqruza5000@gmail.com>
2025-01-18 12:49:34 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,22 @@
+username='Ivan' #имя пользователя
+print('Имя пользователя', username)
+
+title='Main' #заголовок заметки
+print('Заголовок заметки', title)
+
+content='My notes' #описание заметки
+print('Описание заметки', content)
+
+status='In progress' # статус заметки
+print('Статус заметки', status)
+
+created_date='16-01-2025' # дата создания заметки
+print('Дата создания', created_date)
+
+issue_date='25-04-2025' # дата истечения заметки (дедлайн)
+print('Дата истечения заметки (дедлайн)', issue_date)
+
+
+
+
+
